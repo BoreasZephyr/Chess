e274a1cc6321aaccd0aa4f3b8b562526bf1c58fc
Sơn Tùng Nguyễn Văn <nguyenvansontungvt113@gmail.com>
2021-10-21 21:35:39 +0700

    PawnQueening

--- UserInterface/PawnQueening.Designer.cs
+++ UserInterface/PawnQueening.Designer.cs
@@ -30,97 +30,108 @@ namespace UserInterface
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PawnQueening));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.Queenbox = new System.Windows.Forms.PictureBox();
+            this.Bishopbox = new System.Windows.Forms.PictureBox();
+            this.Rookbox = new System.Windows.Forms.PictureBox();
+            this.Knightbox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Queenbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bishopbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rookbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Knightbox)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // Queenbox
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Goldenrod;
-            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.pictureBox1.ErrorImage = null;
-            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(35, 13);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(140, 118);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.Queenbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(154)))), ((int)(((byte)(107)))));
+            this.Queenbox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Queenbox.BackgroundImage")));
+            this.Queenbox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Queenbox.ErrorImage = null;
+            this.Queenbox.InitialImage = ((System.Drawing.Image)(resources.GetObject("Queenbox.InitialImage")));
+            this.Queenbox.Location = new System.Drawing.Point(35, 13);
+            this.Queenbox.Margin = new System.Windows.Forms.Padding(4);
+            this.Queenbox.Name = "Queenbox";
+            this.Queenbox.Size = new System.Drawing.Size(140, 118);
+            this.Queenbox.TabIndex = 1;
+            this.Queenbox.TabStop = false;
+            this.Queenbox.Click += new System.EventHandler(this.Queenbox_Click);
+            this.Queenbox.MouseEnter += new System.EventHandler(this.Queenbox_MouseEnter);
+            this.Queenbox.MouseLeave += new System.EventHandler(this.Queenbox_MouseLeave);
             // 
-            // pictureBox3
+            // Bishopbox
             // 
-            this.pictureBox3.BackColor = System.Drawing.Color.Goldenrod;
-            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
-            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.pictureBox3.ErrorImage = null;
-            this.pictureBox3.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.InitialImage")));
-            this.pictureBox3.Location = new System.Drawing.Point(371, 13);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(140, 118);
-            this.pictureBox3.TabIndex = 1;
-            this.pictureBox3.TabStop = false;
-            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.Bishopbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(154)))), ((int)(((byte)(107)))));
+            this.Bishopbox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Bishopbox.BackgroundImage")));
+            this.Bishopbox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Bishopbox.ErrorImage = null;
+            this.Bishopbox.InitialImage = ((System.Drawing.Image)(resources.GetObject("Bishopbox.InitialImage")));
+            this.Bishopbox.Location = new System.Drawing.Point(371, 13);
+            this.Bishopbox.Margin = new System.Windows.Forms.Padding(4);
+            this.Bishopbox.Name = "Bishopbox";
+            this.Bishopbox.Size = new System.Drawing.Size(140, 118);
+            this.Bishopbox.TabIndex = 1;
+            this.Bishopbox.TabStop = false;
+            this.Bishopbox.Click += new System.EventHandler(this.Bishop_Click);
+            this.Bishopbox.MouseEnter += new System.EventHandler(this.Bishop_MouseEnter);
+            this.Bishopbox.MouseLeave += new System.EventHandler(this.Bishop_MouseLeave);
             // 
-            // pictureBox5
+            // Rookbox
             // 
-            this.pictureBox5.BackColor = System.Drawing.Color.Goldenrod;
-            this.pictureBox5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox5.BackgroundImage")));
-            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.pictureBox5.ErrorImage = null;
-            this.pictureBox5.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox5.InitialImage")));
-            this.pictureBox5.Location = new System.Drawing.Point(205, 13);
-            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(140, 118);
-            this.pictureBox5.TabIndex = 1;
-            this.pictureBox5.TabStop = false;
+            this.Rookbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(154)))), ((int)(((byte)(107)))));
+            this.Rookbox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Rookbox.BackgroundImage")));
+            this.Rookbox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Rookbox.ErrorImage = null;
+            this.Rookbox.InitialImage = ((System.Drawing.Image)(resources.GetObject("Rookbox.InitialImage")));
+            this.Rookbox.Location = new System.Drawing.Point(205, 13);
+            this.Rookbox.Margin = new System.Windows.Forms.Padding(4);
+            this.Rookbox.Name = "Rookbox";
+            this.Rookbox.Size = new System.Drawing.Size(140, 118);
+            this.Rookbox.TabIndex = 1;
+            this.Rookbox.TabStop = false;
+            this.Rookbox.Click += new System.EventHandler(this.Rookbox_Click);
+            this.Rookbox.MouseEnter += new System.EventHandler(this.Rookbox_MouseEnter);
+            this.Rookbox.MouseLeave += new System.EventHandler(this.Rookbox_MouseLeave);
             // 
-            // pictureBox6
+            // Knightbox
             // 
-            this.pictureBox6.BackColor = System.Drawing.Color.Goldenrod;
-            this.pictureBox6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox6.BackgroundImage")));
-            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.pictureBox6.ErrorImage = null;
-            this.pictureBox6.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox6.InitialImage")));
-            this.pictureBox6.Location = new System.Drawing.Point(540, 13);
-            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(140, 118);
-            this.pictureBox6.TabIndex = 1;
-            this.pictureBox6.TabStop = false;
+            this.Knightbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(193)))), ((int)(((byte)(154)))), ((int)(((byte)(107)))));
+            this.Knightbox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Knightbox.BackgroundImage")));
+            this.Knightbox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Knightbox.ErrorImage = null;
+            this.Knightbox.InitialImage = ((System.Drawing.Image)(resources.GetObject("Knightbox.InitialImage")));
+            this.Knightbox.Location = new System.Drawing.Point(540, 13);
+            this.Knightbox.Margin = new System.Windows.Forms.Padding(4);
+            this.Knightbox.Name = "Knightbox";
+            this.Knightbox.Size = new System.Drawing.Size(140, 118);
+            this.Knightbox.TabIndex = 1;
+            this.Knightbox.TabStop = false;
+            this.Knightbox.Click += new System.EventHandler(this.Knightbox_Click);
+            this.Knightbox.MouseEnter += new System.EventHandler(this.Knightbox_MouseEnter);
+            this.Knightbox.MouseLeave += new System.EventHandler(this.Knightbox_MouseLeave);
             // 
             // PawnQueening
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(713, 160);
-            this.Controls.Add(this.pictureBox6);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Knightbox);
+            this.Controls.Add(this.Rookbox);
+            this.Controls.Add(this.Bishopbox);
+            this.Controls.Add(this.Queenbox);
             this.Name = "PawnQueening";
             this.Text = "PawnQueening";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Queenbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bishopbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rookbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Knightbox)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox Queenbox;
+        private System.Windows.Forms.PictureBox Bishopbox;
+        private System.Windows.Forms.PictureBox Rookbox;
+        private System.Windows.Forms.PictureBox Knightbox;
     }
 }
--- UserInterface/PawnQueening.cs
+++ UserInterface/PawnQueening.cs
@@ -7,19 +7,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ChessEngine;
 
 namespace UserInterface
 {
     public partial class PawnQueening : Form
     {
+        public PieceType type;
         public PawnQueening()
         {
             InitializeComponent();
+            this.ControlBox = false;
+            this.Name = "Pawn Queening";
         }
 
-        private void pictureBox3_Click(object sender, EventArgs e)
+        private void Queenbox_Click(object sender, EventArgs e)
         {
-
+            this.type = PieceType.QUEEN;
+            this.Close();
+            this.DialogResult = DialogResult.OK;
         }
+
+        private void Queenbox_MouseLeave(object sender, EventArgs e)
+        {
+            Queenbox.BackColor = Color.FromArgb(193, 154, 107);
+        }
+
+        private void Queenbox_MouseEnter(object sender, EventArgs e)
+        {
+            Queenbox.BackColor = Color.FromArgb(0, 155, 108);
+        }
+        private void Rookbox_Click(object sender, EventArgs e)
+        {
+            this.type = PieceType.ROOK;
+            this.Close();
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void Rookbox_MouseLeave(object sender, EventArgs e)
+        {
+            Rookbox.BackColor = Color.FromArgb(193, 154, 107);
+        }
+
+        private void Rookbox_MouseEnter(object sender, EventArgs e)
+        {
+            Rookbox.BackColor = Color.FromArgb(0, 155, 108);
+        }
+        private void Bishop_Click(object sender, EventArgs e)
+        {
+            this.type = PieceType.BISHOP;
+            this.Close();
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void Bishop_MouseLeave(object sender, EventArgs e)
+        {
+            Bishopbox.BackColor = Color.FromArgb(193, 154, 107);
+        }
+
+        private void Bishop_MouseEnter(object sender, EventArgs e)
+        {
+            Bishopbox.BackColor = Color.FromArgb(0, 155, 108);
+        }
+        private void Knightbox_Click(object sender, EventArgs e)
+        {
+            this.type = PieceType.KNIGHT;
+            this.Close();
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void Knightbox_MouseLeave(object sender, EventArgs e)
+        {
+            Knightbox.BackColor = Color.FromArgb(193, 154, 107);
+        }
+
+        private void Knightbox_MouseEnter(object sender, EventArgs e)
+        {
+            Knightbox.BackColor = Color.FromArgb(0, 155, 108);
+        }
+
     }
 }
