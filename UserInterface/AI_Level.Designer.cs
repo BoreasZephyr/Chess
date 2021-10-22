d4a71ee96089a03a029ed34ce85e071cd7fbfb1a
NGUYỄN HỮU ĐĂNG <ducbmt2009@gmail.com>
2021-10-22 11:13:19 +0700

    difficulty update

--- UserInterface/AI_Level.Designer.cs
+++ UserInterface/AI_Level.Designer.cs
@@ -29,33 +29,17 @@ namespace UserInterface
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.ezLabel = new System.Windows.Forms.Label();
             this.norLabel = new System.Windows.Forms.Label();
             this.hardLabel = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.black = new System.Windows.Forms.PictureBox();
-            this.white = new System.Windows.Forms.PictureBox();
             this.hard = new System.Windows.Forms.PictureBox();
             this.easy = new System.Windows.Forms.PictureBox();
             this.normal = new System.Windows.Forms.PictureBox();
             this.label5 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.black)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.white)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.hard)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.easy)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.normal)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("04b03", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(0, 421);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(400, 54);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "START";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // ezLabel
             // 
@@ -92,44 +76,6 @@ namespace UserInterface
             this.hardLabel.Size = new System.Drawing.Size(57, 19);
             this.hardLabel.TabIndex = 9;
             this.hardLabel.Text = "HARD";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Black;
-            this.label4.Font = new System.Drawing.Font("04b03", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label4.Location = new System.Drawing.Point(30, 284);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(70, 19);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "COLOR:";
-            // 
-            // black
-            // 
-            this.black.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.black.Image = global::UserInterface.Properties.Resources.BPawn_png;
-            this.black.Location = new System.Drawing.Point(225, 317);
-            this.black.Name = "black";
-            this.black.Size = new System.Drawing.Size(82, 82);
-            this.black.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.black.TabIndex = 12;
-            this.black.TabStop = false;
-            this.black.MouseEnter += new System.EventHandler(this.black_MouseEnter);
-            this.black.MouseLeave += new System.EventHandler(this.black_MouseLeave);
-            // 
-            // white
-            // 
-            this.white.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.white.Image = global::UserInterface.Properties.Resources.WPawn_png;
-            this.white.Location = new System.Drawing.Point(91, 317);
-            this.white.Name = "white";
-            this.white.Size = new System.Drawing.Size(84, 82);
-            this.white.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.white.TabIndex = 11;
-            this.white.TabStop = false;
-            this.white.MouseEnter += new System.EventHandler(this.white_MouseEnter);
-            this.white.MouseLeave += new System.EventHandler(this.white_MouseLeave);
             // 
             // hard
             // 
@@ -187,24 +133,18 @@ namespace UserInterface
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(399, 474);
+            this.ClientSize = new System.Drawing.Size(399, 319);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.black);
-            this.Controls.Add(this.white);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.hardLabel);
             this.Controls.Add(this.norLabel);
             this.Controls.Add(this.ezLabel);
             this.Controls.Add(this.hard);
             this.Controls.Add(this.easy);
             this.Controls.Add(this.normal);
-            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "AI_Level";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Setting";
-            ((System.ComponentModel.ISupportInitialize)(this.black)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.white)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.hard)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.easy)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.normal)).EndInit();
@@ -214,16 +154,12 @@ namespace UserInterface
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox normal;
         private System.Windows.Forms.PictureBox easy;
         private System.Windows.Forms.PictureBox hard;
         private System.Windows.Forms.Label ezLabel;
         private System.Windows.Forms.Label norLabel;
         private System.Windows.Forms.Label hardLabel;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.PictureBox white;
-        private System.Windows.Forms.PictureBox black;
         private System.Windows.Forms.Label label5;
     }
 }
